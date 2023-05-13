2c6c6002d539e718cba0aa8001a1012aedc4fdf7
Claudia Afonso <claudia1087@gmail.com>
2023-05-13 13:09:49 +0200

    adds normalize css and next image config

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,19 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
     <main>
       <div className="home">
         <figure>
-          <img
+          <Image
             src="https://static.octopuscdn.com/logos/logo.svg"
             alt="Octopus Energy Logo"
+            width={470}
+            height={67}
+            priority={false}
           />
         </figure>
-        <h1>Welcome to the Octopus Energy Frontend code test!</h1>
+        <h1>Welcome to the Octopus Energy store!</h1>
         <p>
           Get started by visiting the <code>/product</code> URL and editing{" "}
           <code>client/pages/product.js</code>
