59acadcb8cfe8973358c9c0727c1491806bb7704
Claudia Afonso <claudia1087@gmail.com>
2023-05-15 14:40:19 +0200

    readds query in component file

--- client/pages/product/index.tsx
+++ client/pages/product/index.tsx
@@ -1,10 +1,30 @@
-import { useQuery } from "@apollo/client"
+import { useQuery, gql } from "@apollo/client"
 import { Query_ProductsQuery, Product } from "../../src/__generated__/graphql"
 import Loader from "../../components/Loader"
 import Layout from "../../components/Layout"
 import ProductCard from "../../components/Product"
-import { QUERY_PRODUCTS } from "./queries/queries"
 import { ProductCartContextProvider } from "./context/ProductCartContext"
+
+export const QUERY_PRODUCTS = gql`
+  query query_products {
+    allProducts {
+      id
+      name
+      power
+      description
+      price
+      quantity
+      brand
+      weight
+      height
+      width
+      length
+      model_code
+      colour
+      img_url
+    }
+  }
+`
 
 export default function ProductPage() {
   const { data, loading, error } = useQuery<Query_ProductsQuery>(QUERY_PRODUCTS)
